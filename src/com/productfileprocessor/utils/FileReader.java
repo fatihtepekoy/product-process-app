81549e0bae492237cabc889fa3aa142be31eb442
fatih <fatiht@shipmercury.com>
2023-05-17 13:42:54 +0300

    ** file reader is updated

--- src/com/productfileprocessor/utils/FileReader.java
+++ src/com/productfileprocessor/utils/FileReader.java
@@ -1,19 +1,18 @@
 package com.productfileprocessor.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 public class FileReader {
 
   public String readFileContent(File file) {
-    try {
+    try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
       System.out.println("Reading content of " + file.getAbsolutePath());
-      return Files.readAllLines(file.toPath()).get(0);
+      return reader.readLine();
     } catch (IOException e) {
-      e.printStackTrace();
+      throw new RuntimeException(e);
     }
-    return null;
   }
 
 }
